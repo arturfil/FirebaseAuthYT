50fe2ca1ddbf5fc56e962d5a13b2d609ef1a27b3
Arturo Filio Villa <arturfil@hotmail.com>
2019-03-05 12:51:19 -0500

    setup

---
+++ app/src/main/java/com/arturofilio/firebaseauthentication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.arturofilio.firebaseauthentication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_signOut;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_signOut = findViewById(R.id.btn_signout);
+
+        btn_signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
